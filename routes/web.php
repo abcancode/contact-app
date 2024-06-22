0c8af28bca47021a483049d5ef4f363668d8a142
Chidozie Ugwu <jonasbraham@gmail.com>
2024-06-22 19:18:10 +0100

    implement another approach to pass data to view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+function getContacts() {
+
+ return [
+    1 => ['name' => 'Name 1', 'phone' => '1234567890'],
+
+    2 => ['name' => 'Name 2', 'phone' => '2345678901'],
+
+    3 => ['name' => 'Name 3', 'phone' => '3456789012'],
+];
+
+}
+
+
 Route::get('/', function () {
     
     return view('welcome');
@@ -10,13 +24,7 @@ Route::get('/', function () {
     
     Route::get('/contacts', function () {
     
-        $contacts = [
-            1 => ['name' => 'Name 1', 'phone' => '1234567890'],
-
-            2 => ['name' => 'Name 2', 'phone' => '2345678901'],
-
-            3 => ['name' => 'Name 3', 'phone' => '3456789012'],
-        ];
+        $contacts = getContacts();
 
         return view('contacts.index', compact('contacts'));
     
@@ -31,8 +39,12 @@ Route::get('/', function () {
     })->name('contacts.create');
     
     Route::get('/contacts/{id}', function ($id) {
-    
-        return "Contact " . $id ;
+
+        $contacts = getContacts();    
+        abort_unless(isset($contacts[$id]), 404);
+        $contact = $contacts[$id];
+        
+        return view('contacts.show')->with('contact', $contact);
     
     })->name('contacts.show');
     
