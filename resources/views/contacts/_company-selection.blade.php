77d27f09ec508ed1099807aa0396cf9acc53091c
Chidozie Ugwu <jonasbraham@gmail.com>
2024-06-25 13:39:43 +0100

    implement sub-views

---
+++ resources/views/contacts/_company-selection.blade.php
@@ -0,0 +1,8 @@
+<select class="custom-select">
+    <option value="" selected>All Companies</option>
+
+    <?php foreach ($companies as $id => $company): ?>
+    <option value="{{ $id }}">{{ $company['name'] }}</option>
+    <?php endforeach ?>
+  
+  </select>
