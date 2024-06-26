7ef46fcea4af5b7de63b0e8af7d3246ebcdca953
Chidozie Ugwu <jonasbraham@gmail.com>
2024-06-26 23:57:45 +0100

    render views for collection

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Route;
 function getContacts() {
 
  return [
-    1 => ['name' => 'Name 1', 'phone' => '1234567890'],
+    1 => ['id' => 1, 'name' => 'Name 1', 'phone' => '1234567890'],
 
-    2 => ['name' => 'Name 2', 'phone' => '2345678901'],
-
-    3 => ['name' => 'Name 3', 'phone' => '3456789012'],
+    2 => ['id' => 2, 'name' => 'Name 2', 'phone' => '2345678901'],
+    
+    3 => ['id' => 3, 'name' => 'Name 3', 'phone' => '3456789012'],
 ];
 
 }
