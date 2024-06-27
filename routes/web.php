682cafb6bd6933bd6a05a3e62d1347bd826b5284
Chidozie Ugwu <jonasbraham@gmail.com>
2024-06-27 14:49:41 +0100

    create a single action controller

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', WelcomeController::class);
 
-Route::controller(ContactController::class)->name('contacts.')->group(function(){
-    
-    Route::get('/contacts',  'index')->name('index');
-    
-    Route::get('/contacts/create', 'create')->name('create');
-    
-    Route::get('/contacts/{id}',  'show')->name('show');
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 
-});
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+
+Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
