694219e02abbeb628f5c5f7c279bf75f44845795
Chidozie Ugwu <jonasbraham@gmail.com>
2024-07-05 00:16:26 +0100

    create resource controllers

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -15,14 +15,10 @@ class ContactController extends Controller
 
     }
     
-    public function index(CompanyRepository $company, Request $request)
+    public function index()
     {
-        //$companies = [
-        //    1 => ['name' => 'Company One', 'contacts' => 3],
-        //    2 => ['name' => 'Company Two', 'contacts' => 5],
-        //];
+        
         $companies = $this->company->pluck();
-
         $contacts = $this->getContacts();
         return view('contacts.index', compact('contacts', 'companies'));
     }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CompanyController;
+use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', WelcomeController::class);
 
@@ -11,3 +15,9 @@ Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.ind
 Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 
 Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
+
+Route::resource('/companies', CompanyController::class);
+Route::resources([
+    '/tags' => TagController::class,
+    '/tasks' => TaskController::class
+]);
