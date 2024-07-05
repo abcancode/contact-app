4dc61e62d2ca73b24bb6a58e08a3059a03bd674e
Chidozie Ugwu <jonasbraham@gmail.com>
2024-07-05 18:06:29 +0100

    customize the resource route names and parameters

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CompanyController;
@@ -8,8 +8,6 @@ use App\Http\Controllers\TagController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\ContactNoteController;
-
-use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', WelcomeController::class);
@@ -25,13 +23,23 @@ Route::resources([
     '/tags' => TagController::class,
     '/tasks' => TaskController::class
 ]);
-Route::resource('/activities', ActivityController::class)->only([
-    'create', 'store', 'edit', 'update', 'destroy'
-]);
 
-// Alternate method for the except method.
+//Route::resource('/activities', ActivityController::class)->only([
+   // 'create', 'store', 'edit', 'update', 'destroy'
+//]);
+// Alternate method for the only method.
 //Route::resource('/activities', ActivityController::class)->except([
   //'index', 'show'
 //]);
 
 Route::resource('/contacts.notes', ContactNoteController::class)->shallow();
+// Route::resource('/activities', ActivityController::class)->except([
+//     'index', 'show'
+// ]);
+// Route::resource('/activities', ActivityController::class)->names([
+//     'index' => 'activities.all',
+//     'show' => 'activities.view'
+// ]);
+Route::resource('/activities', ActivityController::class)->parameters([
+  'activities' => 'active'
+]);
